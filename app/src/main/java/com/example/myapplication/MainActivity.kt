26738edf6be176dbf44e7142c63abcae246d6f1e
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-09-26 15:15:14 +0300

    Forth task

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,27 +11,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val yellowView = findViewById<View>(R.id.yellowView)
-        val redView = findViewById<View>(R.id.redView)
-        val blueView = findViewById<View>(R.id.blueView)
-
-
-        val screenHeight = resources.displayMetrics.heightPixels.toFloat()
-        val screenWidth = resources.displayMetrics.widthPixels.toFloat()
-
-        val yellowParams = yellowView.layoutParams as LinearLayout.LayoutParams
-        yellowParams.height = (screenHeight * 0.5).toInt()
-        yellowParams.width = (screenWidth * 0.2).toInt()
-        yellowView.layoutParams = yellowParams
-
-        val redParams = redView.layoutParams as LinearLayout.LayoutParams
-        redParams.height = (screenHeight * 0.85).toInt()
-        redParams.width = (screenWidth * 0.55).toInt()
-        redView.layoutParams = redParams
-
-        val blueParams = blueView.layoutParams as LinearLayout.LayoutParams
-        blueParams.height = (screenHeight * 0.8).toInt()
-        blueParams.width = (screenWidth * 0.25).toInt()
-        blueView.layoutParams = blueParams
     }
 }
