4dfeb00e4231d076a3318b723bee41f8177c45e0
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-09-27 11:38:05 +0300

    Fifth task

--- app/src/main/java/com/example/myapplication/BaseScreenActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseScreenActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseScreenActivity : AppCompatActivity() {
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
