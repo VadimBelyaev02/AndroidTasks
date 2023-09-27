53ca80935b4d971ebd64abfdc2d7f762bc292816
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-09-27 09:13:47 +0300

    Fifth task

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,57 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
+import android.widget.ProgressBar
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private val colors = arrayOf(Color.Black, Color.Blue, Color.Cyan, Color.Yellow, Color.Green, Color.Red, Color.Magenta, Color.LightGray,
-        Color(79,3,0), Color(135, 0, 142))
-
-    private lateinit var view: View
-    private lateinit var indexText: TextView
-    private var currentIndex = 0;
+    private lateinit var progressBar: ProgressBar
+    private lateinit var updateButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        indexText = findViewById<TextView>(R.id.currentIndexText)
-        val previousButton = findViewById<Button>(R.id.previousButton)
-        val nextButton = findViewById<Button>(R.id.nextButton)
-        view = findViewById(R.id.customView)
-        view.setBackgroundColor(colors[currentIndex].toArgb())
 
-        indexText.text = currentIndex.toString()
+        progressBar = findViewById(R.id.progressBar)
+        updateButton = findViewById(R.id.updateButton)
 
-
-
-        previousButton.setOnClickListener {
-            previousColor()
-        }
-        nextButton.setOnClickListener {
-            nextColor()
+        updateButton.setOnClickListener {
+            updateProgressBar()
         }
     }
 
-    private fun previousColor() {
-        currentIndex--
-        if (currentIndex < 0) {
-            currentIndex = 9;
-        }
-        view.setBackgroundColor(colors[currentIndex].toArgb())
-        indexText.text = currentIndex.toString()
-    }
-
-    private fun nextColor() {
-        currentIndex++
-        if (currentIndex > 9) {
-            currentIndex = 0
-        }
-        view.setBackgroundColor(colors[currentIndex].toArgb())
-        indexText.text = currentIndex.toString()
+    private fun updateProgressBar() {
+        val randomProgress = Random.nextInt(1, 101)
+        progressBar.animate().duration = 1000
+        progressBar.setProgress(randomProgress, true)
     }
 }
