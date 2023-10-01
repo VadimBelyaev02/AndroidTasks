671ad204e55a41cc083a7e957630a0b2d4118a9e
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-01 12:18:18 +0300

    Developing

--- app/src/main/java/com/example/myapplication/ThirdScreen.kt
+++ app/src/main/java/com/example/myapplication/ThirdScreen.kt
@@ -2,14 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.GridLayout
+import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.cardview.widget.CardView
 import androidx.compose.ui.graphics.Color
 
 class ThirdScreen : AppCompatActivity() {
 
     val elementsAmount = 20
+    val arraySize = 6
 
     private var texts = arrayOf(
         "MY DOCTOR",
@@ -28,6 +36,14 @@ class ThirdScreen : AppCompatActivity() {
         Color(242, 125, 29)
     )
 
+    private var icons = arrayOf(
+        R.drawable.person_add_90,
+        R.drawable.medication_90,
+        R.drawable.fireplace_90,
+        R.drawable.checklist_90,
+        R.drawable.health_and_safety_90,
+        R.drawable.catching_pokemon_90
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,9 +57,30 @@ class ThirdScreen : AppCompatActivity() {
             onBackPressed()
         }
 
-        val tableLayout = findViewById<TableLayout>(R.id.table)
-        val newRow = TableRow(this)
+        val gridLayout = findViewById<GridLayout>(R.id.table)
+        var inflater = LayoutInflater.from(this)
+        for (i in 1..elementsAmount) {
 
-        newRow.add
+            val cardView = inflater.inflate(R.layout.table_card, null) as CardView
+            val cardViewParams = GridLayout.LayoutParams()
+           // cardViewParams.width = resources.getDimensionPixelSize(R.dimen.cardWidth)
+                  cardViewParams.width = 0
+            cardViewParams.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+
+            cardViewParams.height = resources.getDimensionPixelSize(R.dimen.cardHeight)
+            val cardMargin = resources.getDimensionPixelSize(R.dimen.cardMargin)
+            cardViewParams.setMargins(cardMargin, cardMargin, cardMargin, cardMargin)
+
+            cardView.layoutParams = cardViewParams
+            val textView = cardView.findViewById<TextView>(R.id.cardTextView)
+            textView.text = texts[i % arraySize]
+
+            val imageView = cardView.findViewById<ImageView>(R.id.cardImageView)
+            imageView.setImageResource(icons[gridLayout.childCount % arraySize])
+
+
+            gridLayout.addView(cardView)
+
+        }
     }
 }
