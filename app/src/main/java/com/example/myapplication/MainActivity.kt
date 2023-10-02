f9bc9c9a2a57a40d2d7c4c832334504c430faeb3
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-02 22:00:03 +0300

    Developing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,9 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private var progress = 0;
     private lateinit var progressBar: ProgressBar
     private lateinit var updateButton: Button
-    private lateinit var circularProgressBar: CircularProgressBarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,29 +21,10 @@ class MainActivity : AppCompatActivity() {
         progressBar = findViewById(R.id.progressBar)
         updateButton = findViewById(R.id.updateButton)
 
-//        updateButton.setOnClickListener {
-//            updateProgressBar()
-//        }
-        circularProgressBar = findViewById(R.id.circularProgressBar)
-        val startButton = findViewById<Button>(R.id.startButton)
-        startButton.setOnClickListener {
-            val currentProgress = circularProgressBar.getProgress()
-            val newProgress = currentProgress + 10
-            circularProgressBar.setProgress(newProgress)
+        updateButton.setOnClickListener {
+            progress += 10
+            progressBar.progress = progress
+            progressBar.max = 100
         }
-    }
-
-    private fun updateProgressBar() {
-
-        val currentProgress = progressBar.progress
-        val newProgress = currentProgress + 10
-        if (newProgress <= progressBar.max) {
-            progressBar.progress = newProgress
-        }
-
-
-//        val randomProgress = Random.nextInt(1, 101)
-//        progressBar.animate().duration = 1000
-//        progressBar.setProgress(randomProgress, true)
     }
 }
