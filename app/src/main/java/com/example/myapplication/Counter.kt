09985ab4b596a0c40c9a3447c10be640361e41c4
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-03 19:11:16 +0300

    Developing

--- app/src/main/java/com/example/myapplication/Counter.kt
+++ app/src/main/java/com/example/myapplication/Counter.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
 class Counter : AppCompatActivity() {
 
+    private var pauseStartTime: Long = 0
     private lateinit var button: Button
     private lateinit var counterView: CounterView
     private lateinit var counterTextView: TextView
@@ -30,12 +34,56 @@ class Counter : AppCompatActivity() {
         button = findViewById(R.id.counterButton)
         counterView = findViewById(R.id.counterView)
         counterTextView = findViewById(R.id.counterTextView)
-        counterView.value = 0
+
+        val savedValue = savedInstanceState?.getString("counterValue", "0")?.toInt()?:0
+
+        counterView.value = savedValue
+        Log.d("PIZDA", savedValue.toString())
+        counterTextView.text = savedValue.toString()
+
 
         button.setOnClickListener {
             val intent = Intent(this, UpdateCounter::class.java)
             intent.putExtra("currentValue", counterView.value)
             updateCounterLauncher.launch(intent)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        changeBy(5)
+        pauseStartTime = System.currentTimeMillis()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (pauseStartTime != 0.toLong()) {
+            val currentTime = System.currentTimeMillis()
+            val minutesInBackground = ((currentTime - pauseStartTime) / 1000 / 60).toInt()
+            changeBy(2 * (1 - minutesInBackground))
+        }
+    }
+
+    private fun changeBy(diff: Int) {
+        var currentValue = counterView.value
+        currentValue += diff
+        this.counterTextView.text = "$currentValue"
+        counterView.value = currentValue
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val sharedPreferences = getSharedPreferences("storage", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt("textCounter", counterView.value)
+        Log.d("HUI", counterView.value.toString())
+        editor.apply()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.run {
+            putString("counterValue", counterView.value.toString())
         }
     }
 }
